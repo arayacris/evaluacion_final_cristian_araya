90d7c258ef8b4871f10f73b45b37b6abf7464c6e
Cristian Araya <cristian1x2x3x4x@gmail.com>
2025-11-21 22:18:09 -0300

    feat: agregar pipeline CD con acceptance y rollback

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    void testBasico() {
+        assertTrue(true);
+    }
+}
